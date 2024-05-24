f9d09dd45ab6a79d7471d31314d8e92d3c6e3ca3
Evgeny Skopin <79599538+scopchik@users.noreply.github.com>
2024-05-24 20:22:13 +0300

    Update WireMovement.cs

--- Assets/Scripts/WireMovement.cs
+++ Assets/Scripts/WireMovement.cs
@@ -4,45 +4,35 @@ public class WireMovement : MonoBehaviour
 {
     private bool isDragging = false;
     [SerializeField] private Transform attachedPort;
-
-    // Вызывается при нажатии на левую кнопку мыши над объектом провода
+    
     private void OnMouseDown()
     {
         isDragging = true;
     }
-
-    // Вызывается при отпускании левой кнопки мыши
+    
     private void OnMouseUp()
     {
         isDragging = false;
-
-        // При отпускании кнопки мыши проверяем, находится ли провод над портом,
-        // и если да, то перемещаем его к порту
         if (attachedPort != null)
         {
             transform.position = attachedPort.position;
         }
     }
 
-    // Вызывается при столкновении с другим коллайдером
     private void OnTriggerEnter(Collider other)
     {
-        // Проверяем, столкнулся ли провод с портом
         if (other.CompareTag("Port"))
         {
-            // Присваиваем порт, с которым столкнулся провод
             attachedPort = other.transform;
         }
     }
 
-    // Вызывается каждый кадр
     private void Update()
     {
-        // Если провод перетаскивают, то обновляем его позицию в каждом кадре
         if (isDragging)
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f; // Устанавливаем z-координату равной 0, чтобы объект не двигался вдоль оси z
+            mousePosition.z = 0f;
             transform.position = mousePosition;
         }
     }
